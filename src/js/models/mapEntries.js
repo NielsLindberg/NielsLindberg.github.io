830eea79d8977b36aba381280a42f27c827dd97e
Niels Lindberg <eighthourcream@gmail.com>
2016-08-28 18:53:20 +0200

    started implementing knockout

---
+++ src/js/models/mapEntries.js
@@ -0,0 +1,9 @@
+var mapEntries = [{
+    name: "Islands Brygge",
+    description: "City harbor bath",
+    tags: ['leasure', 'bathing', 'beachvolley', 'sports']
+}, {
+    name: "Kebabistan",
+    description: "Best Vesterbro Shawarma",
+    tags: ['food', 'turkish', 'fastfood']
+}];
