b7cc7e1d81b506141c62af68423bccdcf2673503
Niels Lindberg <eighthourcream@gmail.com>
2016-09-04 13:38:03 +0200

    trying to fix initMap error

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,17 @@
 var map;
+
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
-        center: copenhagenGeo,
+        center: {
+            lat: 55.6761,
+            lng: 12.5683
+        },
         zoom: 14
     });
     var infoWindow = new google.maps.InfoWindow({
-        content: ''
+        content: '123'
     });
-    MapEntryViewModel["Symbol(_latestValue)"].forEach(function(mapEntry) {
+    "MapEntryViewModel.Symbol(_latestValue)".forEach(function(mapEntry) {
         mapEntry.addMarkerData();
     });
 }
